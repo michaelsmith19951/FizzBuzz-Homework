0f94556c892f198d52219a343b3b57f7bb66ccc9
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-22 11:39:36 -0400

    1st failure

--- tdd_fizzbuzz_homework.rb
+++ tdd_fizzbuzz_homework.rb
@@ -1,17 +1,2 @@
-
-
 def fizzbuzz()
-	fizzemptyarr = []
-	fizzarr = [*1..100]
-	fizzarr.each do |item|
-		if item % 3 and 5 == 0
-			item = "Mined Minds"
-		elsif item % 3 == 0
-			item = "Mined"
-		elsif item % 5 == 0
-			item = "Mines"
-		end
-		fizzemptyarr.push(item)
-	end
-	fizzemptyarr
 end
