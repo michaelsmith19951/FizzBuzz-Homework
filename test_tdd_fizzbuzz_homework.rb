804f8c9ed261b1554b64d885b531712a37e98e4c
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-22 11:12:57 -0400

    Fixed some errors

---
+++ test_tdd_fizzbuzz_homework.rb
@@ -0,0 +1,27 @@
+require "minitest/autorun"
+require_relative "tdd_fizzbuzz_homework.rb"
+
+class TestTddFizzBuzzHomework < Minitest::Test
+
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+       def test_assert_that_end_result_is_array
+        assert_equal(Array, fizzbuzz().class)
+    end
+
+    def test_assert_array_length_is_100
+        assert_equal(100, fizzbuzz().length)
+    end
+
+    def test_assert_first_position_is_1
+        assert_equal(1, fizzbuzz().first)
+    end
+
+    def test_3rd_elemet_equals_mined
+    	assert_equal("Mined", fizzbuzz()[2])
+    end
+end
+
+
