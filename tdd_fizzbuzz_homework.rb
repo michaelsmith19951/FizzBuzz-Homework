f378365b222cd75ad612bb4267fbb8a7d090918e
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-22 12:43:40 -0400

    10th test that failed

--- tdd_fizzbuzz_homework.rb
+++ tdd_fizzbuzz_homework.rb
@@ -4,7 +4,7 @@ def fizzbuzz()
 	fizzarr = [*1..100]
 	fizzarr[2] = "Mined"
 	fizzarr[4] = "Minds"
-	fizzarr[14] = "Mined Minds"
+	fizzarr[15] = "Mined Minds"
 	fizzarr.each do |item|
 		if item %3 == 0
 			item = "Mined"
