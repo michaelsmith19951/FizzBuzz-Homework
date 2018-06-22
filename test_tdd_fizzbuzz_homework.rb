7bb818a5011e08e89661934771793c9b62b15bc7
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-22 11:42:40 -0400

    2nd test that failed

--- test_tdd_fizzbuzz_homework.rb
+++ test_tdd_fizzbuzz_homework.rb
@@ -6,4 +6,8 @@ class TestTddFizzBuzzHomework < Minitest::Test
 	def  test_if_array_is_a_class
 		assert_equal(Array, fizzbuzz().class)
 	end
+
+	def test_assert_array_length_is_100
+        assert_equal(100, fizzbuzz().length)
+    end 
 end
